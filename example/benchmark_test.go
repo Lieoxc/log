f3b32a7c38a170dee78290c43fc62051f3f8c4a1
lieoxc <xiaocongli45@gmail.com>
2022-07-24 22:19:48 +0800

    基准测试与logx进行对比

--- example/benchmark_test.go
+++ example/benchmark_test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"log"
 	"testing"
 
 	logs "github.com/Lieoxc/log"
+	"github.com/zeromicro/go-zero/core/logx"
 )
 
 func Benchmark_infoLog(b *testing.B) {
@@ -12,9 +12,9 @@ func Benchmark_infoLog(b *testing.B) {
 		logs.Info("123")
 	}
 }
-func Benchmark_systemLog(b *testing.B) {
+func Benchmark_logx(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
-		log.Println("123")
+		logx.Info("123")
 	}
 }
